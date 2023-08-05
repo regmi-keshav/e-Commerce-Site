8da5ec55806145363a3a651cf4241f26f1d8fd61
keshav_regmi <keshavregmi70@gmail.com>
2023-08-05 10:14:24 +0530

    React and dependencies setup

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+        Hello World!!
+    </div>
+  );
+}
+
+export default App;
